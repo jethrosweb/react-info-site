984d168e3bd4db9f0ff166e688ecc8c0e56c112b
Jethro Dean <jethroldean@gmail.com>
2022-03-26 17:32:33 +1100

    Completed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,12 @@
 import React from "react"
+import logo from "../images/logo192.png"
 
 export default function Navbar() {
     return (
-        <h1>Navbar goes here</h1>
+        <nav>
+            <img src={logo} alt="Logo" className="nav--icon" />
+            <h3 className="nav--logo_text">ReactFacts</h3>
+            <h4 className="nav--title">React Course - Project 1</h4>
+        </nav>
     )
 }
